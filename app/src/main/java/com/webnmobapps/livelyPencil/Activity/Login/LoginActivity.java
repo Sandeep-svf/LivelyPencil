cdea69d1118042f7cbbe3864858a7622847ad7df
Sandeep Maurya <svf.sandeep@gmail.com>
2022-10-13 18:58:15 +0530

    New Flow Chnages Started From Here 23 Sept 2022

--- app/src/main/java/com/webnmobapps/livelyPencil/Activity/Login/LoginActivity.java
+++ app/src/main/java/com/webnmobapps/livelyPencil/Activity/Login/LoginActivity.java
@@ -399,7 +399,7 @@ public class LoginActivity extends AppCompatActivity {
                             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // To clean up all activities
                             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                             startActivity(intent);
-                            // Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+                             Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
 
 
                         } else {
